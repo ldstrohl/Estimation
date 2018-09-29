4b2176b5f1a00911ecef958c1d6a38e149d75e7a
Unknown <ldstrohl@gmail.com>
2018-09-28 21:45:42 -0700

    Architechted Kalman filter
    python implementation of linear bayeseian estimator (inital commit)

--- Python/KF.py
+++ Python/KF.py
@@ -1,1 +1,25 @@
-print("Hello World!")
+def KF(A, B, H, Q, R, u, z, xprev, Pprev):
+    xdim = size(A, 1);
+    % zdim = size(H, 2);
+    if nargin < 8
+        x0 = zeros(xdim, 1);
+        P = eye(xdim);
+    elseif
+    nargin < 9
+    x0 = zeros(xdim, 1);
+
+
+	end
+
+	% filter
+	xpred = A * x0 + B * u;
+	Ppred = A * P * A
+	' + Q;
+	ytilde = z - H * xpred;
+	S = H * Ppred * H
+	'+R;
+	K = Ppred * H
+	'/S;
+	x = xpred + K * ytilde;
+	P = (eye(2) - K * H) * P;
+
