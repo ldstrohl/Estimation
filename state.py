08be8897a013e82a9cb5893af53beb987d234d27
Lloyd Strohl <lloyd.strohl@sslmda.com>
2018-10-01 15:17:07 -0700

    Cleaned up comments

--- state.py
+++ state.py
@@ -9,6 +9,4 @@ class State:
     def __init__(self, x0, p0):
         self.x = x0
         self.P = p0
-    # @classmethod
-    # def New(cls,*args, **kwargs):
-    #     return State(*args, **kwargs)
+
